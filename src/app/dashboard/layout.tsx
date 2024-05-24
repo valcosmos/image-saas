ef0e3886ef74f310b42033d6423b156d319cf5e6
Cupid Valentine <westerngod@126.com>
2024-05-24 23:32:14 +0800

    feat: add avatar and nav bar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,26 @@
 import { redirect } from 'next/navigation'
 import { getServerSession } from '@/server/auth'
+import { Button } from '@/components/ui/Button'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/Avatar'
 
-export default async function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function DashboardLayout({ children, nav }: Readonly<{ children: React.ReactNode, nav: React.ReactNode }>) {
   const session = await getServerSession()
   if (!session?.user)
     redirect('/api/auth/signin')
-  return children
+  return (
+    <div className="h-screen">
+      <nav className="relative h-20 flex justify-end items-center border-b">
+        <Button variant="ghost">
+          <Avatar>
+            <AvatarImage src={session.user.image!} />
+            <AvatarFallback>{ session.user?.name?.substring(0, 2) }</AvatarFallback>
+          </Avatar>
+        </Button>
+        <div className="absolute h-full left-1/2 -translate-x-1/2 flex justify-center items-center">
+          { nav}
+        </div>
+      </nav>
+      <main className="h-[calc(100%-80px)]">{children}</main>
+    </div>
+  )
 }
